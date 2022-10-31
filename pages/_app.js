420b558cc81636e9de267192ad2297b3afc36dc9
AntoDeca1 <antoniodecandia01@gmail.com>
2022-10-31 21:21:03 +0100

    change9

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import Layout from '../components/layout';
 import '../styles/globals.css';
+import '@fontsource/inter';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
 
 function MyApp({ Component, pageProps }) {
   return (
